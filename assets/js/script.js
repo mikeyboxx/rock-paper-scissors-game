64e5b212b0187b7d9bf91aa82ebec4bd006f1856
Benjamin Wright <bwright@trilogyed.com>
2022-10-02 13:24:54 -0500

    Refactored game and included JSON.stringify output

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,114 @@
-const PLAYER_ONE = 0;
-const PLAYER_TWO = 1;
-const TIE = -1;
+// --------------------- RPS Object ------------------------
+function RPS() {
+  this.player1 = null
+  this.player2 = null
+  this.games = []
+}
+
+RPS.prototype.play = function() {
+  var playerName = prompt('Please enter your name.');
+  var computerPlayers = ["Deep Blue", "Alpha Zero", "Hal 9000"];
+  this.player1 = new Player(playerName, true);          // create a human player
+  this.player2 = new Player(computerPlayers[Math.floor(Math.random() * computerPlayers.length)], false); // create a non-human player
+  this.newGame();
+}
+
+RPS.prototype.newGame = function() {
+  this.games.push(new Game(this.player1, this.player2, (this.games.length - 1)));
+  console.log(`${this.player1.name} vs. ${this.player2.name}`)
+  this.games[this.games.length - 1].playRounds();
+  if (confirm('Do you want to play again?')) {
+    return this.newGame();
+  }
+  var scores = {
+    [this.player1.name]: 0,
+    [this.player2.name]: 0,
+    tie: 0,
+    rounds: 0
+  }
+
+  this.games.forEach(game => {
+    game.rounds.forEach(round => {
+      console.log(round.winner)
+      scores[round.winner]++;
+      scores.rounds++
+    })
+  })
+
+  alert(`Player ONE stats\n----------------------
+Wins: ${scores[this.player1.name]}
+Losses: ${scores.rounds - (scores[this.player1.name] + scores.tie)}
+Ties: ${scores.tie}
+
+Player TWO stats\n----------------------
+Wins: ${scores[this.player2.name]}
+Losses: ${scores.rounds - (scores[this.player1.name] + scores.tie)}
+Ties: ${scores.tie}
+
+Number of games played: ${this.games.length}
+Number of rounds played: ${scores.rounds}`); 
+}
 
 // --------------------- Game Object -----------------------
 // Game object that accepts nbr of rounds each game should be
-function Game(player1, player2){
-    this.players = [player1, player2];  // array of Player objects. idx 0 is player1, idx 1 is player 2
-    this.scorecard = [0, 0, 0];         // idx 0 is player1 wins, idx 1 is player 2 wins, idx 3 are ties
-    this.rounds = [];                   // array of Round objects
-    this.winner = null;                 // 0 is player1, 1 is player2, -1 is a tie
-    this.maxNbrOfRounds = 1;            // maximim number of rounds in the game
-    this.nbrOfGamesPlayed = 0;
+function Game(player1, player2, gameNbr){
+  var nbrOfRounds = parseInt(prompt('Please enter number of rounds'));
+  while (nbrOfRounds !== nbrOfRounds)   // check for NaN parseint will return NaN if not numeric
+    nbrOfRounds = parseInt(prompt(`Invalid Input!  Try Again!
+Please enter number of rounds`));
+  this.player1 = player1,  // array of Player objects.      // idx 0 is player1 wins, idx 1 is player 2 wins, idx 3 are ties
+  this.player2 = player2,
+  this.rounds = [];                   // array of Round objects
+  this.winner = null;                 // 0 is player1, 1 is player2, -1 is a tie
+  this.maxNbrOfRounds = nbrOfRounds;            // maximim number of rounds in the game
+  this.gameNbr = 0;
 }
-// reset the scoreboard, winner, and max nbr of rounds
-Game.prototype.reset = function (nbrOfRounds){
-    this.scorecard = [0, 0, 0];      
-    this.winner = null;    
-    this.maxNbrOfRounds = nbrOfRounds;            
-}    
+
 // adds Round object to the rounds array
 Game.prototype.addRound = function (round){
-    this.rounds.push(round);
+  var round = new Round(this.player1.draw(), this.player2.draw(), this);
+  this.rounds.push(round);
+  console.log("Round:", round)
 }
+
 // determine who the game's winner is 0 is player1, 1 is player 2, -1 is a tie
-Game.prototype.determineGameWinner = function (){
-    if(this.scorecard[PLAYER_ONE] > this.scorecard[PLAYER_TWO]){
-        this.winner = PLAYER_ONE;
-    } else if (this.scorecard[PLAYER_ONE] < this.scorecard[PLAYER_TWO]){
-        this.winner = PLAYER_TWO;
-    } else this.winner = TIE;
+Game.prototype.determineGameWinner = function() {
+  var playerOneName = this.player1.name;
+  var playerTwoName = this.player2.name;
+  var playerOneWins = this.rounds.filter(round => round.winner === playerOneName ).length;
+  var playerTwoWins = this.rounds.filter(round => round.winner === playerTwoName ).length;
+  var ties = this.rounds.length - (playerOneWins + playerTwoWins);
+  if (playerOneWins > playerTwoWins){
+      this.winner = this.player1.name;
+  } else if (playerOneWins < playerTwoWins){
+      this.winner = this.player2.name;
+  } else this.winner = "tie";
+
+  alert(`
+Player ONE: (${this.player1.human ? "human": "computer" } ) ${playerOneWins} wins.
+Player TWO: (${this.player2.human ? "human": "computer" }) ${playerTwoWins} wins.
+There were ${ties} ties
+
+${(this.winner === "tie")?"It's a tie!":`Player ${playerOneWins > playerTwoWins ? this.player1.name : this.player2.name} is the WINNER!`}`); 
 }
 
-// update Player object stats and the scoreboard array
-Game.prototype.updateStats = function (){
-    var winner = this.rounds[this.rounds.length-1].winner;   // current round's winner
-    
-    if (winner === TIE) {                  // winner = -1 means it's a tie
-        this.scorecard[2]++;               // increment scorecard's nbr of ties (idx 2 holds ties)
-        this.players[PLAYER_ONE].ties++;       // increment player 1 nbr of ties
-        this.players[PLAYER_TWO].ties++;       // increment player 2 nbr of ties
-    } else {
-        this.scorecard[winner]++;          // increment scorecard's nbr of wins (idx 0 or 1 holds wins)           
-        this.players[winner].wins++;       // increment player's wins
-        this.players[winner ^ 1].losses++; // incr player lossses; XOR will give you opposite nbr. 0 ^ 1 = 1, 1 ^ 0 = 0
-    }
-    this.nbrOfRoundsPlayed++;              // increment number of rounds         
-}
 
+Game.prototype.playRounds = function () {
+  while (this.rounds.length < this.maxNbrOfRounds) {
+    this.addRound();
+    this.rounds[this.rounds.length - 1].determineRoundWinner();
+  }
+  this.determineGameWinner();
+  alert("Game Over")
+}
 
 // --------------------- Player Object -----------------------
 // Player object that accepts player's name and a flag to tell whether the player is human or not
 function Player(name, humanFlag){
     this.name = name;
     this.human = humanFlag;
-    this.humanText = (this.human)?'human':'computer';
-    this.wins = 0;
-    this.losses = 0;
-    this.ties = 0;
 }
+
 // if human, then prompt for input, if not human, then generate random input.
 Player.prototype.draw = function (){
     var userInput = '';
@@ -76,82 +127,51 @@ Player.prototype.draw = function (){
 
 // --------------------- Round Object -----------------------
 // Round object, that accepts player1's and player2's draws, and a Game identifier
-function Round(playerOneDraw, playerTwoDraw, gameId){
-    this.gameId = gameId;
-    this.draw = [playerOneDraw, playerTwoDraw];  // array of player's draws
-    this.winner;                                 // 0 = p1, 1 = p2, -1 = tie
+function Round(playerOneDraw, playerTwoDraw, game) {
+    this.game = game;
+    this.playerOneDraw = playerOneDraw;
+    this.playerTwoDraw = playerTwoDraw; // array of player's draws
+    this.winner;                        // a player's name
 }
+
 // this determines who the winner is for each round
 Round.prototype.determineRoundWinner = function (){
-    var rules = [['R', 'S'], ['S', 'P'], ['P', 'R']];      // Game rules. 1st element beats 2nd element..
+  var rules = [['R', 'S'], ['S', 'P'], ['P', 'R']];      // Game rules. 1st element beats 2nd element..
+  
+  if (this.playerOneDraw === this.playerTwoDraw){  // its a tie
+      this.winner = "tie";                                 // value of -1 is a tie
+  } else {
+      // search the rule array to find the rule that contains player1's draw and player2's draw
+      var rule = rules.find(element => element[0] === this.playerOneDraw &&
+                                        element[1] === this.playerTwoDraw);
+      (rule) ? this.winner = this.game.player1.name : this.winner = this.game.player2.name;   // if rule is found then player 1 wins, else player 2 wins
+  }
+  alert(`
+Player ONE (${this.game.player1.human ? "human": "computer" }) had: ${this.playerOneDraw}
+Player TWO (${this.game.player2.human ? "human": "computer"}) had: ${this.playerTwoDraw}
     
-    if (this.draw[PLAYER_ONE] === this.draw[PLAYER_TWO]){  // its a tie
-        this.winner = TIE;                                 // value of -1 is a tie
-    } else {
-        // search the rule array to find the rule that contains player1's draw and player2's draw
-        var rule = rules.find(element => element[0] === this.draw[PLAYER_ONE] &&
-                                         element[1] === this.draw[PLAYER_TWO]);
-        (rule) ? this.winner = PLAYER_ONE : this.winner = PLAYER_TWO;   // if rule is found then player 1 wins, else player 2 wins
-    }
+${(this.winner === "tie")?"It's a tie!":`Player ${this.winner} is the WINNER!`}`); 
 }
 
+var rps = new RPS();
+rps.play();
+
+// safely handles circular references
+JSON.safeStringify = (obj, indent = 2) => {
+  let cache = [];
+  const retVal = JSON.stringify(
+    obj,
+    (key, value) =>
+      typeof value === "object" && value !== null
+        ? cache.includes(value)
+          ? undefined // Duplicate reference found, discard key
+          : cache.push(value) && value // Store value in our collection
+        : value,
+    indent
+  );
+  cache = null;
+  return retVal;
+};
 
 
-
-// ------- main logic -----------
-function playGame(game) {
-    var playAgain = true;
-    var gameNbr = 0;
-    
-    while (playAgain) {
-        gameNbr++;      
-        var nbrOfRounds = parseInt(prompt('Please enter number of rounds'));
-        while (nbrOfRounds !== nbrOfRounds)   // check for NaN parseint will return NaN if not numeric
-            nbrOfRounds = parseInt(prompt(`Invalid Input!  Try Again!\nPlease enter number of rounds`));
-        
-        game.reset(nbrOfRounds);     // reset the scoreboard, winner, and number of rounds
-        
-        // loop for the number of rounds defined for the game
-        for (var i = 0; i < game.maxNbrOfRounds; i++){
-            var round = new Round(game.players[PLAYER_ONE].draw(), game.players[PLAYER_TWO].draw(), gameNbr);  // create a new Round 
-            round.determineRoundWinner();            // determine the winner of the round
-            game.addRound(round);                    // add the Round Object to the Game
-            game.updateStats();                      // update the Game and Player's stats
-            // display the winner of the round and both players draws
-            alert(`\nPlayer ONE (${game.players[PLAYER_ONE].humanText}) had: ${round.draw[PLAYER_ONE]}` +
-                 `\nPlayer TWO (${game.players[PLAYER_TWO].humanText}) had: ${round.draw[PLAYER_TWO]}` +
-                 `\n\n${(round.winner === TIE)?"It's a tie!":`Player ${round.winner + 1} is the WINNER!`}`); 
-        }
-        
-        game.determineGameWinner();  // determine the winner of the game
-
-        // display the winner of the game, both players wins, and total ties
-        alert(`\nPlayer ONE: (${game.players[PLAYER_ONE].humanText} ) ${game.scorecard[PLAYER_ONE]} wins.` +
-              `\nPlayer TWO: (${game.players[PLAYER_TWO].humanText}) ${game.scorecard[PLAYER_TWO]} wins.` +
-              `\nThere were ${game.scorecard[2]} ties` +
-              `\n\n${(game.winner === TIE)?"It's a tie!":`Player ${game.winner + 1} is the WINNER!`}`); 
-
-        playAgain = confirm('Do you want to play again?');
-    }
-
-    game.nbrOfGamesPlayed = gameNbr;   // set nbr of games played
-
-    alert(`Player ONE stats\n----------------------` +
-          `\nWins: ${game.players[PLAYER_ONE].wins}` +
-          `\nLosses: ${game.players[PLAYER_ONE].losses}` +
-          `\nTies: ${game.players[PLAYER_ONE].ties}` +
-          `\n\nPlayer TWO stats\n----------------------` +
-          `\nWins: ${game.players[PLAYER_TWO].wins}` +
-          `\nLosses: ${game.players[PLAYER_TWO].losses}` +
-          `\nTies: ${game.players[PLAYER_TWO].ties}` +
-          `\n\nNumber of games played: ${game.nbrOfGamesPlayed}` +
-          `\nNumber of rounds played: ${game.rounds.length}`); 
-}
-
-
-var playerName = prompt('Please enter your name.');
-var player1 = new Player(playerName, true);          // create a human player
-var player2 = new Player('Big Blue', false);         // create a non-human player
-var game = new Game(player1, player2);               // create a new Game 
-
-playGame(game);
+console.log(JSON.safeStringify(rps, 2));
